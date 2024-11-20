775d9c04f025c2f6db1c5e2d7af1c31506477622
Khadija02-gif <b.dija.2002@gmail.com>
2024-11-20 16:16:21 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -11,9 +11,10 @@ app = Flask(__name__)
 def hello_world():
     return render_template('hello.html') #comm2
   
-@app.route("/contact/")
-def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"
+  
+  @app.route("/contact/")
+def contact():
+    return render_template("contact.html")
   
 @app.route('/tawarano/')
 def meteo():
@@ -35,9 +36,6 @@ def mongraphique():
 def histogramme():
     return render_template("histogramme.html")
   
-  @app.route("/contact/")
-def contact():
-    return render_template("contact.html")
 
 
                                                                                                                                       
